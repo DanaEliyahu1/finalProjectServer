3fcab23013ebd8ed071816969511ddbde8f036aa
נמרוד חייט <haiatn@auth.ad.bgu.ac.il>
2019-09-04 18:13:52 +0300

    SEARCH

--- app.js
+++ app.js
@@ -2,39 +2,30 @@
 
 const express = require("express");
 const app = express();
-var cors=require("cors");
-const mainModule= require("./mainModule");
+var cors = require("cors");
+const mainModule = require("./mainModule");
 const port = process.env.PORT || 3000; //environment variable
 var DButils = require('./DButils');
 
 
 var bodyParser = require('body-parser')
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
+    extended: true
 }));
 //app.use(express.json());
 app.use(cors());
-app.use(express.json(),function(req, res, next) {
-	express.json();
+app.use(express.json(), function (req, res, next) {
+    express.json();
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
 
-
-//server side fronend
-//app.use(express.static('D:\\documents\\users\\danaeliy\\Downloads\\ASS_3.3-master/ASS_3.3-master'))
-/*
-app.get("/test", async (req, res) => {
-    res.send("geneAns");
-});
-
-*/
-function sqlQuery(query){
-    var promise= new Promise(function(resolve,reject){
-       DButils.db.all(query,[], async(err,rows)=>{
+function sqlQuery(query) {
+    var promise = new Promise(function (resolve, reject) {
+        DButils.db.all(query, [], async (err, rows) => {
             resolve(rows);
         });
     });
@@ -44,20 +35,37 @@ function sqlQuery(query){
 
 
 
-app.get("/querySearch/:inputGene",(req, res) => {
-    var geneAns=undefined;
-    sqlQuery("SELECT gene_symbol FROM Genes WHERE gene_symbol = '" + req.params.inputGene + "'").then(function(result){
-        geneAns=result;
-        res.send(JSON.stringify (geneAns));
-        //if gene exists create info JSON
-        
-        //if gene does not exist 
-                //try in the synonyms (need splits by '; ')
-                //if exist create info JSON
-                //else error not found
-        console.log("querySearch");
+app.get("/querySearch/:inputGene", async (req, res) => {
+
+    var geneAns =await sqlQuery("SELECT gene_symbol FROM Genes WHERE gene_symbol = '" + req.params.inputGene + "'");
+    if (geneAns.length != 0) {
+        console.log(geneAns);
+        res.send(geneAns);
+        console.log("querySearch1");
+        return;
+    }
+    else {
+        geneAns = await sqlQuery("SELECT * FROM Genes WHERE synonyms LIKE  '%" + req.params.inputGene + "%'");
+
+        if (geneAns.length != 0) {
+
+            for (var i = 0; i < geneAns.length; i++) {
+                var geneSynonyms = geneAns[i].synonyms.split("; ");
+                for (var j = 0; j < geneSynonyms.length; j++) {
+                    if (geneSynonyms[j] == req.params.inputGene) {
+                        res.send(JSON.stringify(geneAns[i]));
+                        console.log("querySearch2");
+                        return;
+                    }
+
+                }
+            }
+
+        }
+        console.log("querySearch3");
+        res.send(undefined);
+    }
     });
-});
 
 
 
